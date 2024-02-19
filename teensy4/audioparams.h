7fea66ef6e267cd5b94eecd2e0a3afaed988aa88
J-tech0 <56737975+J-tech0@users.noreply.github.com>
2024-02-19 05:18:39 +0000

    correct comments

--- teensy4/audioparams.h
+++ teensy4/audioparams.h
@@ -12,9 +12,9 @@
 
 #if SAMPLE_RATE_PARAM == 48000
     #define AUDIO_SAMPLE_RATE_EXACT 48000.0f            //AudioStream.h, sample rate
-    #define AUDIO_TX_SIZE         192                   //usb_desc.h, buffer size dependent on sample rate, 4*45
+    #define AUDIO_TX_SIZE         192                   //usb_desc.h, buffer size dependent on sample rate, 4*48
     #define AUDIO_RX_SIZE         192
-    #define USB_AUDIO_FEEDBACK_ACCUMULATOR 805306368    //usb_audio.cpp, feedback_accumulator, 2^24*44.1khz
+    #define USB_AUDIO_FEEDBACK_ACCUMULATOR 805306368    //usb_audio.cpp, feedback_accumulator, 2^24*48khz
     #define USB_AUDIO_SAMPLE_RATE_TARGET 48000          //usb_audio.cpp, USB buffer target
     #define AUDIO_INTERFACE_BUFFER 48000                //usb_audio.cpp, usb.c
     #define USB_DESC_SAMPLERATE 48000                   //usb_desc.c
